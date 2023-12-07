098523c914a51ad0745d805419a297926e97ebf4
JaspreetRFSingh <jaspreetsinghtuli@gmail.com>
2023-12-07 09:00:00 +0530

    Add CORS configuration

---
+++ src/main/java/com/timeseries/config/WebConfig.java
@@ -0,0 +1,10 @@
+package com.timeseries.config;
+import org.springframework.context.annotation.*;
+import org.springframework.web.servlet.config.annotation.*;
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**").allowedOrigins("*");
+    }
+}
